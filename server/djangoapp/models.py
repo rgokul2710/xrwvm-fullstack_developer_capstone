41eb179a21212f59a0efe5305dd7b7033e3e2f6c
rgokul2710 <rgokul2710@gmail.com>
2024-11-23 09:49:50 -0500

    module 5

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -6,22 +6,28 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    country_of_origin = models.CharField(max_length=100, blank=True, null=True)  # New field
+    country_of_origin = models.CharField(max_length=100, 
+        blank=True, 
+        null=True)  # New field
     established_year = models.IntegerField(
         validators=[
-            MinValueValidator(1800),  # Cars likely started being produced after 1800
+            MinValueValidator(1800),
             MaxValueValidator(now().year)
         ],
         blank=True, null=True  # Optional field
     )
-    logo = models.ImageField(upload_to='car_make_logos/', blank=True, null=True)  # Optional logo field
+    logo = models.ImageField(upload_to='car_make_logos/', 
+        blank=True, 
+        null=True)  # Optional logo field
 
     def __str__(self):
         return self.name  # Return the name as the string representation
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name="car_models")  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, 
+        on_delete=models.CASCADE, 
+        related_name="car_models")  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -53,11 +59,16 @@ class CarModel(models.Model):
         default='PETROL'
     )  # New field
     seating_capacity = models.IntegerField(default=5)  # New field
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)  # Price with 2 decimal places
-    color_options = models.CharField(max_length=255, blank=True, null=True)  # Store color options as a comma-separated string
+    price = models.DecimalField(max_digits=10, 
+        decimal_places=2, 
+        blank=True, 
+        null=True)
+    color_options = models.CharField(max_length=255, 
+        blank=True, 
+        null=True)
     fuel_efficiency = models.DecimalField(
         max_digits=5, decimal_places=2, blank=True, null=True
     )  # Fuel efficiency in km/l or equivalent
 
     def __str__(self):
-        return f"{self.name} ({self.car_make.name})"  # More descriptive string representation
+        return f"{self.name} ({self.car_make.name})"  
